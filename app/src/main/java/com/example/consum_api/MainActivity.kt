0857c705e4867f4d20d4b6e4c662641b869aac56
Lmore07 <lmoreirat@uteq.edu.ec>
2022-06-13 23:40:20 -0500

    final

--- app/src/main/java/com/example/consum_api/MainActivity.kt
+++ app/src/main/java/com/example/consum_api/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.consum_api
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONArray
 import org.json.JSONObject
@@ -18,8 +18,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
     fun consumir_api(view:View){
         val textView = findViewById<TextView>(R.id.respuesta_api);
+        var rpta:String;
         try{
             // Instantiate the RequestQueue.
             val queue = Volley.newRequestQueue(this)
@@ -28,7 +30,22 @@ class MainActivity : AppCompatActivity() {
             // Request a string response from the provided URL.
             val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
                 Response.Listener { response ->
-                    textView.text = response["data"].toString()
+                    rpta=response.getString("data")
+                    Log.i("","")
+                    var lstususarios = ""
+                    val JSONlista = JSONArray(rpta)
+                    for (i in 0 until JSONlista.length()) {
+                        val usuario: JSONObject = JSONlista.getJSONObject(i)
+                        lstususarios = lstususarios +
+                        "\n"+"id: "+usuario.getString("id")+
+                        "; name: "+usuario.getString("name")+
+                        "; email: "+usuario.getString("email")+
+                        "; gender: "+usuario.getString("gender")+
+                        "; status: "+usuario.getString("status") +"\n"
+                        }
+
+                    textView.setText(lstususarios)
+
                 },
                 Response.ErrorListener { error ->
                     // TODO: Handle error
