2cd879e6b34c7f2b2edc259132c64a78a9c7deb5
Lmore07 <lmoreirat@uteq.edu.ec>
2022-06-13 22:20:23 -0500

    cambios

--- app/src/main/java/com/example/consum_api/MainActivity.kt
+++ app/src/main/java/com/example/consum_api/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.consum_api
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun consumir_api(view:View){
+        val textView = findViewById<TextView>(R.id.respuesta_api);
+        try{
+            // Instantiate the RequestQueue.
+            val queue = Volley.newRequestQueue(this)
+            val url = "https://gorest.co.in/public/v1/users"
+
+            // Request a string response from the provided URL.
+            val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                Response.Listener { response ->
+                    textView.text = response["data"].toString()
+                },
+                Response.ErrorListener { error ->
+                    // TODO: Handle error
+                }
+            )
+            queue.add(jsonObjectRequest);
+        }catch (e:Exception){
+            textView.setText(e.message);
+        }
+
     }
 }
